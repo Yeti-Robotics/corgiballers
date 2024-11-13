13bd1bf2261170ef72255ab736352cb941dd1d62
qwertycloudhub <vihaansden@gmail.com>
2024-11-12 20:26:38 -0500

    more polish

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
